09fdf7ae370177b2ce4a531a16432b430270fa4e
Priyanka24-net <priyankamc@netanalytiks.com>
2025-03-04 10:22:21 +0530

    changed the department classifier function

--- main_library/department_classifier.py
+++ main_library/department_classifier.py
@@ -1,24 +1,20 @@
-from flask import request, jsonify
 from main_library.model import DepartmentClassifier
 
-
-def handle_user_message():
+def department_detection(user_message):
     """
     Handles user messages by classifying the department and responding accordingly.
     """    
     # Initialize classifiers
     department_classifier = DepartmentClassifier(
         model_path='main_library/dp_classifier_model.h5',
-        embedding_model_name='all-MiniLM-L6-v2',
-        labels_path='main_library/labels.json'
+        embedding_model_name='all-MiniLM-L6-v2', # put your absolute path
+        labels_path='main_library/labels.json' # put your absolute path
     )
 
-    # Parse user message from UI
-    data = request.json
-    user_message = data.get('text', '').strip()
-    # print(user_message)
-        
-    processing_message = user_message
+    # Ensure input is stripped of leading/trailing spaces
+    processing_message = user_message.strip()
+    
+    # Predict department
     department = department_classifier.predict_department(processing_message)
     # print(f"Detected Department: {department}")
     
